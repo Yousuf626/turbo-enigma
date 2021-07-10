1fa5cf473bbcd818bdf07d9dddcd61b535d3cc18
Yousuf <yousuf0626@gmail.com>
2021-07-10 18:37:47 +0500

    Multiply Function

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test{
+    public void addNum(int x, int y){
+        System.out.println(x+y);
+    }
+     public void subNum(int x, int y){
+        System.out.println(x-y);
+    }
+     public void mulNum(int x, int y){
+       
+    }
+     public void addNum(int x, int y){
+     
+    }
+}
